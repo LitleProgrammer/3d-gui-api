f403529a70ed50cfaa1c792bf4ced59ad2fc8bd0
linusglimm <linus.glimm06@gmail.com>
2024-05-03 17:39:47 +0200

    Initial commit

---
+++ src/main/java/de/littleprogrammer/guiapi/Api.java
@@ -0,0 +1,17 @@
+package de.littleprogrammer.guiapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Api extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
